71e95b6560d6974570c017392504bbcf337cd449
gerardbelvis <gerard.belvis@gmail.com>
2021-04-23 13:08:05 +0200

    required cambio para tecnologia

--- src/models/Vacuna.ts
+++ src/models/Vacuna.ts
@@ -3,7 +3,7 @@ import { Schema, model, Document, Types, Mongoose } from "mongoose";
 const VacunaSchema: Schema = new Schema({
   name: { type: String, required: true },
   description: { type: String },
-  tecnology: { type: String },
+  tecnology: { type: String, required: true},
   acceptationdate: { type: Date},
   
 });
